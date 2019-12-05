bc19f2814eaa0d200848849f64cc5818806a11e4
Rahul Shah <rahulshah7@outlook.com>
2019-12-05 21:52:27 +0000

    Change FizzBuzz to es6 class

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,22 +1,22 @@
-function FizzBuzz() {}
+class FizzBuzz {
+  play(number) {
+    let result = "";
+    let isMultiple = false;
 
-FizzBuzz.prototype.play = function(number) {
-  let result = "";
-  let isMultiple = false;
+    if (number % 3 === 0) {
+      result += "Fizz";
+      isMultiple = true;
+    }
 
-  if (number % 3 === 0) {
-    result += "Fizz";
-    isMultiple = true;
+    if (number % 5 === 0) {
+      result += "Buzz";
+      isMultiple = true;
+    }
+
+    if (!isMultiple) {
+      result += String(number);
+    }
+
+    return result;
   }
-
-  if (number % 5 === 0) {
-    result += "Buzz";
-    isMultiple = true;
-  }
-
-  if (!isMultiple) {
-    result += String(number);
-  }
-
-  return result;
-};
+}
